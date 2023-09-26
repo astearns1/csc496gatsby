3dd7f44e940cf6890c853c320b928b55843ad86d
Alan Stearns <alan_stearns2@mymail.eku.edu>
2023-09-26 17:41:59 +0000

    Initial project version

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `CSC 496 Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
