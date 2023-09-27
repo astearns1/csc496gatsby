d93de46673ee123af6daf365658459c5a449c800
Alan Stearns <alan_stearns2@mymail.eku.edu>
2023-09-27 01:35:19 +0000

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,8 @@ module.exports = {
     title: `CSC 496 Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
